228c8a4d25aeb0388ea3b2c87b3fd25c7eca65c2
oreakinwole <oreakinwole@gmail.com>
2022-03-23 15:48:30 +0100

    web edits

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+/* Author: Ore Akinwole for Jiffix Technologies */
+
+$(document).ready(function () {
+  $(".jael-slick-carousel").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
